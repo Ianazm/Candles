dd4594da5b8c7eedfad630d6a352fb05bbeaf147
Yana <yanazm88@gmail.com>
2023-08-14 22:17:41 +0300

    Безліч нового коду
    Додали фіксуючу кнопку "ВГОРУ", фото

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,4 @@
+// МОДАЛКА
 (() => {
     const refs = {
       openModalBtn: document.querySelector('[data-modal-open]'),
@@ -12,3 +13,21 @@
       refs.modal.classList.toggle('is-hidden');
     }
   })();
+
+
+  // КНОПКА ПРОКРУТКІ НАВЕРХ
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+}
+
